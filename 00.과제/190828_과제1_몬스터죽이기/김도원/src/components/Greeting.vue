f4a4f5965452f59c3f95c4dd0cfe2125651216d7
Haneul Cho <stuykr@naver.com>
2019-08-28 16:56:41 +0900

    도원님 과제1 처음 버전 제출

---
+++ 00.과제/190828_과제1_몬스터죽이기/김도원/src/components/Greeting.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="hello">
+		<h2>간단 북마크 안내</h2>
+		<br>
+		<p>LocalStorage를 활용한 간단 북마크입니다.</p>
+		<p>브라우저 쿠키 및 캐시를 삭제하지 않는 한 북마크 데이터가 영구 보존됩니다.</p>	
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Greeting',
+	data : () => ({
+		message : "hi?",
+		age : '3',
+		itemList : [
+			{ name : '사과', key: 'red'},
+			{ name : '배'},
+			{ name : '바나나'},
+			{ name : '과자'}
+		],
+		isShowing : false,
+		message1 : "보인다",
+		message2 : "안보인다"
+	}),
+	methods : {
+		handleClick(){
+			this.message = 'HIHI!'
+		},
+		sayHi(){
+
+		},
+		toggle(){
+			this.isShowing = !this.isShowing
+		}
+	}
+}
+</script>
---
+++ 00.과제/190828_과제1_몬스터죽이기/김도원/src/components/HomeworkView.vue
@@ -0,0 +1,50 @@
+<template>
+	<div>
+		<div>야생의 {{name}}</div>
+		<div>HP : {{hp}}</div>
+		<div class="bar">
+			<el-progress :text-inside="true" :stroke-width="26" :percentage="hp"></el-progress>
+		</div>
+		<el-button type="primary" round @click="hitMontster">때리기</el-button>
+		<div v-if="isDied">
+			<img src="@/assets/cookie_2.jpg" alt="">
+			<p>{{name}}가 {{txt1}}</p>
+			<el-button type="danger" round @click="hpReset">쿠키몬스터 살리기</el-button>
+		</div>
+		<div v-else>
+			<img src="@/assets/cookie_1.jpg" alt="">
+			<p>{{name}}가 {{txt2}}</p>
+			<el-button type="danger" round disabled>쿠키몬스터 살리기</el-button>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	data : () => ({
+		name: "쿠키몬스터",
+		hp: 100,		
+		isDied : false,
+		txt1 : "죽고 말았습니다...아아아 ㅜㅜㅜ",
+		txt2 : "아직 살아있어요 뿌샤뿌샤"
+	}),
+	methods : {
+		hitMontster(){			
+			if( this.hp > 0 && !this.isDied ){
+				this.hp -= 10
+				if( this.hp === 0){
+					this.isDied = true;
+				}
+			}
+		},
+		hpReset(){			
+			this.hp = 100;
+			this.isDied = false;
+		}
+	}
+}
+</script>
+
+<style scoped>
+	.bar { width: 600px;}
+</style>
