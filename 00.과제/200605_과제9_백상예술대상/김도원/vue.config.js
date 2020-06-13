f06c9022ba5791495b9520b68304b5f2d8be862c
Haneul Cho <stuykr@naver.com>
2020-06-13 11:09:50 +0900

    [과제피드백] 백상예술대상 과제 웹팩설정 수정

--- 00.과제/200605_과제9_백상예술대상/김도원/vue.config.js
+++ 00.과제/200605_과제9_백상예술대상/김도원/vue.config.js
@@ -2,6 +2,11 @@ const devMode = process.env.NODE_ENV !== 'production'
 const path = require('path')
 
 module.exports = {
+	filenameHashing: false,
+	productionSourceMap: false,
+	css: {
+		extract: true
+	},
 	devServer: {
 		proxy: {
 			'/api': {
@@ -11,5 +16,34 @@ module.exports = {
 				changeOrigin: true,
 			}
 		}
+	},
+	chainWebpack: config => {
+		config.plugins.delete('preload')
+		config.plugins.delete('prefetch')
+		config
+			.plugin('extract-css')
+			.tap(([options, ...args]) => [
+				Object.assign({}, options, {
+					filename: 'css/style.css'
+				}),
+				...args
+			])
+
+		config.module
+			.rule('fonts')
+			.test(/\.(ttf|otf|eot|woff|woff2)$/)
+			.use('url-loader')
+			.loader('url-loader')
+			.tap(options => ({
+				...options,
+				limit: -1,
+				fallback: {
+					loader: 'file-loader',
+					options: {
+						name: 'fonts/[name].[ext]'
+					}
+				}
+			}))
+
 	}
 }
